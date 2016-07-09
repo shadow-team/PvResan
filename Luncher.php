2a643a93512c90e7c24cdfda752db0721035f1e7
mobiniuuu <mobinfalahpor7@gmail.com>
2016-07-09 16:33:53 -0700

    Update Luncher.php

--- Luncher.php
+++ Luncher.php
@@ -1,5 +1,5 @@
 <?php
-define('BOT_TOKEN', '**TOKEN**');
+define('BOT_TOKEN', '**143473753:AAGx73CDSP3sYZrXPfETmsoP_QhsWOGKhN4**');
 define('API_URL', 'https://api.telegram.org/bot'.BOT_TOKEN.'/');
 
 function apiRequestWebhook($method, $parameters) {
